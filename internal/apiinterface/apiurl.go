d7f44b8b658cff3985640738f868335c9175c936
James Stanton <jestanton@outlook.com>
2020-05-07 16:51:49 +0100

    grouped const variables into const block

--- internal/apiinterface/apiurl.go
+++ internal/apiinterface/apiurl.go
@@ -7,14 +7,16 @@ package apiinterface
 
 import "fmt"
 
-// apiURL is the URL of the Blade II Online REST API.
-const apiURL = "https://rahvicrr4g.execute-api.eu-west-2.amazonaws.com"
+const (
+	// apiURL is the URL of the Blade II Online REST API.
+	apiURL = "https://rahvicrr4g.execute-api.eu-west-2.amazonaws.com"
 
-// apiVersion is the target Amazon API Gateway stage that should be accessed.
-const apiVersion = "v1"
+	// apiVersion is the target Amazon API Gateway stage that should be accessed.
+	apiVersion = "v1"
 
-// endpointProfiles is the path of the profiles endpoint of the Blade II Online REST API.
-const endpointProfiles = "profiles"
+	// endpointProfiles is the path of the profiles endpoint of the Blade II Online REST API.
+	endpointProfiles = "profiles"
+)
 
 // GetURL constructs and returns the URL for the specified endpoint of the Blade II Online REST API.
 func GetURL(endpoint string) string {
