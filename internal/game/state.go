e7002fab826a62b3d9762a466aff30231b4bcb81
James Stanton <jestanton@outlook.com>
2020-05-07 23:59:05 +0100

    Fixed package comment

--- internal/game/state.go
+++ internal/game/state.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT license
 // that can be found in the LICENSE.md file.
 
-// Package game provides implements the Blade II Online game server.
+// Package game implements the Blade II Online game server.
 package game
 
 // Player is a typedef for the two different players
